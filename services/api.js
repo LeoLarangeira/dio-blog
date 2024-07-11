71a90611b3e13c2dd738a13b23dd81447feebaa8
LeoLarangeira <leonardo.alves.larangeira@gmail.com>
2024-07-11 16:16:53 -0300

    minor changes

--- services/api.js
+++ services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const api  = axios.create({
-    baseURL: 'https://nuareafrukmnjnaakplk.supabase.co/rest/v1',
+    baseURL: 'https://buzfeinfhqnwhmeggspv.supabase.co/rest/v1',
     headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA"
+        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ1emZlaW5maHFud2htZWdnc3B2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyMDU1OTQyMCwiZXhwIjoyMDM2MTM1NDIwfQ.8fkn1FiaG80Rajjo5dWF3azz0mmdNVWJMRl79p02Oeg",
+        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ1emZlaW5maHFud2htZWdnc3B2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyMDU1OTQyMCwiZXhwIjoyMDM2MTM1NDIwfQ.8fkn1FiaG80Rajjo5dWF3azz0mmdNVWJMRl79p02Oeg"
     }
 })
--- utils/mdx-utils.js
+++ utils/mdx-utils.js
@@ -2,7 +2,7 @@ import { api } from '../services/api'
 
 export const getPosts = async () => {
     const {data} = await api.get('/posts'); 
-
+    console.log("--------------DATA-------------", data)
     if(data){
         return data;
     }
@@ -10,10 +10,17 @@ export const getPosts = async () => {
     return []
 }
 
+
 export const getPostBySlug = async (id) => {
-
-    //TODO: BUSCAR UM POST EM ESPECIFICO.
-    //const {data} = await api.get(`/post?id=eq.${id}`)
-
-    return {}
-}
+    try {
+        const { data } = await api.get(`/posts?id=eq.${id}`);
+        if (data && data.length > 0) {
+            console.log("--------------DATA-------------", data)
+            return data[0]; // Retorna o primeiro post encontrado com o ID especificado
+        }
+        return {}; // Retorna um objeto vazio se nenhum post for encontrado
+    } catch (error) {
+        console.error('Erro ao buscar o post:', error);
+        throw error; // Lança o erro para que ele possa ser tratado em outro lugar, se necessário
+    }
+};
