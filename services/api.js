42f27c846a2d1f16a78d27320cc8f9a940c1e3c3
LeoLarangeira <leonardo.alves.larangeira@gmail.com>
2024-07-12 13:43:33 -0300

    minor changes

--- services/api.js
+++ services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const api  = axios.create({
-    baseURL: 'https://buzfeinfhqnwhmeggspv.supabase.co/rest/v1',
+    baseURL: "",
     headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ1emZlaW5maHFud2htZWdnc3B2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyMDU1OTQyMCwiZXhwIjoyMDM2MTM1NDIwfQ.8fkn1FiaG80Rajjo5dWF3azz0mmdNVWJMRl79p02Oeg",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ1emZlaW5maHFud2htZWdnc3B2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyMDU1OTQyMCwiZXhwIjoyMDM2MTM1NDIwfQ.8fkn1FiaG80Rajjo5dWF3azz0mmdNVWJMRl79p02Oeg"
+        apikey: "" ,
+        authorization: ""
     }
 })
